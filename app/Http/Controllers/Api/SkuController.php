f7568348d5e5d768a0cb43bb920f6556ced7c01e
sasha <sashapozhidaev07@example.com>
2022-07-30 17:31:20 +0600

    api sku

--- app/Http/Controllers/Api/SkuController.php
+++ app/Http/Controllers/Api/SkuController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Models\Sku;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class SkuController extends Controller
 {
-    public function getSku()
+    public function getSku(): Response
     {
-        return Sku::with('product')->latest()->get();
+        return response()->json(Sku::with('product')->latest()->get());
     }
 }
